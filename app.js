73b5aa8f16e6f7f032028cd5f522062d06cc3755
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-01-21 21:45:09 -0500

    add promise for api call

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
 const express = require('express');
-const steem = require('steem');
-
 const app = express();
 
+const steem = require('steem');
 steem.api.setOptions({ url: 'https://api.steemit.com' });
 
-let jeffData;
-
-steem.api.getAccountHistory('jeffbernst', -1, 1035, function(err, result) {
-  jeffData = result;
-});
-
-function totalTransfers(data) {
+function totalPowerUps(data) {
   let total = 0;
-  for (let i = 0; i < jeffData.length; i++) {
-    if (jeffData[i][1].op[0] === 'transfer_to_vesting') {
-      let transferString = jeffData[i][1].op[1].amount;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][1].op[0] === 'transfer_to_vesting') {
+      let transferString = data[i][1].op[1].amount;
       total += Number(transferString.substr(0, transferString.indexOf(' ')));
     }
   }
   return total.toString();
 }
 
+let jeffHistoryPromise = new Promise((resolve, reject) => {
+  steem.api.getAccountHistory('jeffbernst', -1, 1000, function(err, result) {
+    if (err) reject(err);
+    resolve(result);
+  });
+});
+
+let totalPowerUpsVal;
+
+jeffHistoryPromise
+  .then(data => {
+    totalPowerUpsVal = totalPowerUps(data);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
 app.get('/', (req, res) => {
-  res.send(totalTransfers(jeffData));
+  res.send(totalPowerUpsVal);
 });
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
+
+// function totalPowerUps(data) {
+//   return data
+//     .filter(trx => trx[1].op[0] === 'transfer_to_vesting')
+//     .reduce((acc, cur) => {
+//       return (
+//         acc + Number(cur[1].op[0].amount.substr(0, cur.amount.indexOf(' ')))
+//       );
+//     });
+// }
